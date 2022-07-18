6ff5ed84e4937d86b38c720d37fdd2ba1e894380
Dushyant <devops@bydushyant.com>
2022-07-18 11:00:34 +0530

    Fix theme toggle button

--- main.js
+++ main.js
@@ -1,47 +1,27 @@
-// const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-// const currentTheme = localStorage.getItem('theme');
-
-// if (currentTheme) {
-//     document.documentElement.setAttribute('data-theme', currentTheme);
-  
-//     if (currentTheme === 'dark') {
-//         toggleSwitch.checked = true;
-//     }
-// }
-
-// function switchTheme(e) {
-//     if (e.target.checked) {
-//         document.documentElement.setAttribute('data-theme', 'dark');
-//         localStorage.setItem('theme', 'dark');
-//     }
-//     else {        document.documentElement.setAttribute('data-theme', 'light');
-//           localStorage.setItem('theme', 'light');
-//     }    
-// }
-
-// toggleSwitch.addEventListener('change', switchTheme, false);
-
 const themeToggle = document.querySelector("#theme-toggle");
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'dark') {
-        document.body.classList.remove("dark-theme");
-        document.body.classList.add("light-theme");
-    }
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.contains("light-theme")
+    ? enableDarkMode()
+    : enableLightMode();
+});
+
+function enableDarkMode() {
+  document.body.classList.remove("light-theme");
+  document.body.classList.add("dark-theme");
 }
 
-themeToggle.addEventListener('click', () => {
-    if (localStorage.getItem('theme') === 'light') {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-        document.body.classList.remove("light-theme");
-        document.body.classList.add("dark-theme");
-    }
-    else {        
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-        document.body.classList.remove("dark-theme");
-        document.body.classList.add("light-theme");
-    }
-});
+function enableLightMode() {
+  document.body.classList.remove("dark-theme");
+  document.body.classList.add("light-theme");
+}
+
+function setThemePreference() {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    enableDarkMode();
+    return;
+  }
+  enableLightMode();
+}
+
+document.onload = setThemePreference();
